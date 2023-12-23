b161789aa95f700f990d9f685bfec1c8a1f290dc
Devendra Dhakad <121676745+Devendradhakad12@users.noreply.github.com>
2023-12-23 18:14:56 +0530

    first commit

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from "react";
+import "./About.css";
+import SimpleImageSlider from "react-simple-image-slider";
+import { AboutContent } from "../../utils/utilities";
+import { SliderImages } from "../../utils/utilities";
+
+const AboutUs = () => {
+  /* This State and functions are responsible for responsive about page  */
+  const [windowSize, setWindowSize] = useState(getWindowSize());
+  function getWindowSize() {
+    const { innerWidth, innerHeight } = window;
+    return { innerWidth, innerHeight };
+  }
+  useEffect(() => {
+    function handleWindowResize() {
+      setWindowSize(getWindowSize());
+    }
+    window.addEventListener("resize", handleWindowResize);
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
+  return (
+    <div className="about-page-container">
+      <h2 className="about-heading">About Us</h2>
+      {/* About Images and Text */}
+      {AboutContent &&
+        AboutContent?.map((item, index) => (
+          <div
+            key={index}
+            className="about-card"
+            style={{
+              flexDirection:
+                windowSize.innerWidth > 650 && index % 2 === 0 && "row-reverse",
+            }}
+          >
+            <div className="image-div">
+              <img src={item.image} alt="" />
+            </div>
+            <div className="about-text-div">
+              <h2>{item.heading}</h2>
+              <p>{item.aboutText}</p>
+            </div>
+          </div>
+        ))}
+
+      {/* Image Slider */}
+      <div className="image-slider-div">
+        <h3>Our Offers</h3>
+        <SimpleImageSlider
+          width={windowSize.innerWidth > 650 ? 796 : 400}
+          height={windowSize.innerWidth > 650 ? 404 : 200}
+          images={SliderImages}
+          showBullets={true}
+          showNavs={true}
+          style={{ borderRadius: "20px" }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
